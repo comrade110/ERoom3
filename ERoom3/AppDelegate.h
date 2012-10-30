c2e9a4ef9b130487923b1e9255e875da43cae223
Lowson Chen <comrade213@sina.com>
2012-10-30 11:16:00 +0800

    Initial Commit

---
+++ ERoom3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ERoom3
+//
+//  Created by user on 12-10-30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
